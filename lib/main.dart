b00084d9d7a627d67bbe64162c617a529bb2a183
puchochiri <doawishfor@naver.com>
2023-11-28 21:49:08 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,59 @@ void typeFunction() {
 
 }
 
+void typeBool() {
+  String name = "빛날";
+  int isInt = 10;
+  double isDouble = 2.5;
+  bool isTrue = true;
+
+  print(name);
+  print(isInt);
+  print(isDouble);
+  print(isTrue);
+}
+
+void typeList() {
+  List<String> blackPinkList = ['리사','지수','제니','로제'];
+
+  print(blackPinkList);
+  print(blackPinkList[1]);
+  print(blackPinkList[2]);
+  print(blackPinkList.length);
+  print(blackPinkList);
+  //행이동 알트 시프트 화살표
+
+  blackPinkList.add('빛날');
+  print(blackPinkList);
+
+  final newList = blackPinkList.where(
+      (name) => name == '리사' || name == '지수', // '리사' 또는 '지수'만 유지
+  );
+  print(newList);
+  print(newList.toList()); // Iterable을 List로 다시 변환할 때 .toList() 사용
+
+  blackPinkList = ['리사','지수','제니','로제'];
+
+  final newBlackPink = blackPinkList.map(
+      (name) => '블랙핑크 $name', // 리스트의 모든 값 앞에 '블랙핑크' 추가
+  );
+  print(newBlackPink);
+
+  print(newBlackPink.toList());
+
+  //리스트를 순회하며 갓들을 더함
+  final allMembers = blackPinkList.reduce((value, element) => value + ',' + element);
+
+  print(allMembers);
+
+  final allMembersFold = blackPinkList.fold<int>(0, (value, element) => value + element.length);
+
+  print(allMembersFold);
+
+}
+
+
 void main() {
-  dateTimeFunction();
+  typeList();
 }
 // dart lib/main.dart
